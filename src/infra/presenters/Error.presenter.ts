4a0e8011beb54cb1b7dfff788a86589a970c8d84
Luzin7 <lvictor.dutra@gmail.com>
2024-07-12 22:49:22 -0300

    feat: add more errors in errormap

--- src/infra/presenters/Error.presenter.ts
+++ src/infra/presenters/Error.presenter.ts
@@ -1,8 +1,10 @@
 import {
   BadRequestException,
+  ConflictException,
   ForbiddenException,
   InternalServerErrorException,
   NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { ServiceError } from '@shared/core/errors/ServiceError';
 import { statusCode } from '@shared/core/types/statusCode';
@@ -12,11 +14,13 @@ import { statusCode } from '@shared/core/types/statusCode';
  * @class ErrorPresenter - Present all possible application errors
  */
 export class ErrorPresenter {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private static errorMap: { [key: number]: any } = {
     [statusCode.NOT_FOUND]: NotFoundException,
-    [statusCode.INTERNAL_SERVER_ERROR]: InternalServerErrorException,
     [statusCode.BAD_REQUEST]: BadRequestException,
     [statusCode.FORBIDDEN]: ForbiddenException,
+    [statusCode.UNAUTHORIZED]: UnauthorizedException,
+    [statusCode.CONFLICT]: ConflictException,
   };
 
   static toHTTP(error: ServiceError) {
