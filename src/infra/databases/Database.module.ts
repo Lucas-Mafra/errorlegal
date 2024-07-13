69687d1423b62eb5a876fa279de8f39dfb3ff231
Luzin7 <lvictor.dutra@gmail.com>
2024-07-12 22:46:15 -0300

    feat: add refresh token repository

--- src/infra/databases/Database.module.ts
+++ src/infra/databases/Database.module.ts
@@ -1,6 +1,8 @@
-import { PlayerRepository } from '@modules/player/repositories/PlayerRepository';
+import { PlayerRepository } from '@modules/player/repositories/contracts/PlayerRepository';
+import { RefreshTokensRepository } from '@modules/player/repositories/contracts/RefreshTokenRepository';
 import { Module } from '@nestjs/common';
 import { PlayerRepositoryImplementation } from './prisma/player/PlayerRepository';
+import { RefreshTokensRepositoryImplementation } from './prisma/player/RefreshTokensRepositoryImplementation';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
@@ -10,7 +12,11 @@ import { PrismaService } from './prisma/prisma.service';
       provide: PlayerRepository,
       useClass: PlayerRepositoryImplementation,
     },
+    {
+      provide: RefreshTokensRepository,
+      useClass: RefreshTokensRepositoryImplementation,
+    },
   ],
-  exports: [PrismaService, PlayerRepository],
+  exports: [PrismaService, PlayerRepository, RefreshTokensRepository],
 })
 export class DatabaseModule {}
