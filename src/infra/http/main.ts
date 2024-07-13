916ae3cf71f2c133c47621f1a37d1369bb46bf40
Luzin7 <lvictor.dutra@gmail.com>
2024-07-13 18:20:03 -0300

    feat: add cors config and helmet

--- src/infra/http/main.ts
+++ src/infra/http/main.ts
@@ -1,10 +1,21 @@
 import { env } from '@infra/env';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const origin = env.NODE_ENV === 'production' ? env.PROD_URL : env.DEV_URL;
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin,
+    credentials: true,
+    methods: 'GET,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization', 'refresh_token'],
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Fable to Table')
