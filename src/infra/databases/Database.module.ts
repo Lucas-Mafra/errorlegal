dc26aa8002200871a694a924db095873d2dba82b
Luzin7 <lvictor.dutra@gmail.com>
2024-07-13 18:17:45 -0300

    feat: add game repository

--- src/infra/databases/Database.module.ts
+++ src/infra/databases/Database.module.ts
@@ -1,6 +1,8 @@
+import { GameRepository } from '@modules/game/repositories/contracts/GameRepository';
 import { PlayerRepository } from '@modules/player/repositories/contracts/PlayerRepository';
 import { RefreshTokensRepository } from '@modules/player/repositories/contracts/RefreshTokenRepository';
 import { Module } from '@nestjs/common';
+import { GameRepositoryImplementation } from './prisma/game/GameRepository';
 import { PlayerRepositoryImplementation } from './prisma/player/PlayerRepository';
 import { RefreshTokensRepositoryImplementation } from './prisma/player/RefreshTokensRepositoryImplementation';
 import { PrismaService } from './prisma/prisma.service';
@@ -16,7 +18,16 @@ import { PrismaService } from './prisma/prisma.service';
       provide: RefreshTokensRepository,
       useClass: RefreshTokensRepositoryImplementation,
     },
+    {
+      provide: GameRepository,
+      useClass: GameRepositoryImplementation,
+    },
   ],
-  exports: [PrismaService, PlayerRepository, RefreshTokensRepository],
+  exports: [
+    PrismaService,
+    PlayerRepository,
+    RefreshTokensRepository,
+    GameRepository,
+  ],
 })
 export class DatabaseModule {}
