361eb74a8fac0c1dec2785c3f9b11f4fdbde2a58
Luzin7 <lvictor.dutra@gmail.com>
2024-07-12 22:53:02 -0300

    feat: create gateway

--- src/modules/player/gateways/LoginPlayer.gateway.ts
+++ src/modules/player/gateways/LoginPlayer.gateway.ts
@@ -1,9 +1,9 @@
 import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
 import { z } from 'zod';
 
-const createPlayerSchema = z.object({
+const loginPlayerSchema = z.object({
   name: z.string().trim().min(4).max(20).max(255),
   password: z.string().trim().min(8).max(255),
 });
 
-export const CreatePlayerGateway = new ZodValidationPipe(createPlayerSchema);
+export const LoginPlayerGateway = new ZodValidationPipe(loginPlayerSchema);
