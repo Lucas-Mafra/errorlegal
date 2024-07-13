565fdb88744bad54a52de32b094950af9d337aa6
Luzin7 <lvictor.dutra@gmail.com>
2024-07-12 22:47:53 -0300

    feat: add cryptography module

--- src/infra/http/app.module.ts
+++ src/infra/http/app.module.ts
@@ -1,10 +1,15 @@
 import { PlayerModule } from '@modules/player/Player.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '@providers/auth/Auth.module';
+import { CryptographyModule } from '@providers/cryptography/Cryptography.module';
 
 @Module({
-  imports: [AuthModule, PlayerModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    CryptographyModule,
+    PlayerModule,
+  ],
 })
 export class AppModule {}
