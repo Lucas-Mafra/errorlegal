ff5777e8d839732d7d04a0e54050dc292397e2ba
Luzin7 <lvictor.dutra@gmail.com>
2024-07-13 21:20:59 -0300

    feat: update module

--- src/modules/game/Game.module.ts
+++ src/modules/game/Game.module.ts
@@ -1,11 +1,27 @@
 import { DatabaseModule } from '@infra/databases/Database.module';
 import { Module } from '@nestjs/common';
 import { CreateGameController } from './controllers/CreateGame.controller';
+import { DeleteGameController } from './controllers/DeleteGame.controller';
+import { FindGameByIdController } from './controllers/FindGameById.controller';
+import { UpdateGameController } from './controllers/UpdateGame.controller';
 import { CreateGameService } from './services/CreateGame.service';
+import { DeleteGameService } from './services/DeleteGame.service';
+import { FindGameByIdService } from './services/FindGameById.service';
+import { UpdateGameService } from './services/UpdateGame.service';
 
 @Module({
-  controllers: [CreateGameController],
+  controllers: [
+    CreateGameController,
+    DeleteGameController,
+    FindGameByIdController,
+    UpdateGameController,
+  ],
   imports: [DatabaseModule],
-  providers: [CreateGameService],
+  providers: [
+    CreateGameService,
+    DeleteGameService,
+    FindGameByIdService,
+    UpdateGameService,
+  ],
 })
 export class GameModule {}
