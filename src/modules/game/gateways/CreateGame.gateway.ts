cfc88b05ea1710832996ff517bc03d9fbfdeee7b
Luzin7 <lvictor.dutra@gmail.com>
2024-07-13 22:35:02 -0300

    feat: create gateway

--- src/modules/game/gateways/CreateGame.gateway.ts
+++ src/modules/game/gateways/CreateGame.gateway.ts
@@ -2,7 +2,7 @@ import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
 import { z } from 'zod';
 
 const createGameSchema = z.object({
-  name: z.string().trim().min(4).max(20),
+  name: z.string().trim().min(4).max(150),
   description: z.string().trim().min(4).max(255),
   imageUrl: z.string().url().nullable(),
   masterId: z.number(),
--- src/modules/game/gateways/FindGameById.gateway.ts
+++ src/modules/game/gateways/FindGameById.gateway.ts
@@ -2,7 +2,7 @@ import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
 import { z } from 'zod';
 
 const findGameByIdSchema = z.object({
-  gameId: z.coerce.number(),
+  gameId: z.string(),
 });
 
 export const FindGameByIdGateway = new ZodValidationPipe(findGameByIdSchema);
