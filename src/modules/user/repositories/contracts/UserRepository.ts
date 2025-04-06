60962f8ceb443c818b96b4905a4bcc9b53f0517e
Luan Victor <lvictor.dutra@gmail.com>
2025-04-06 16:59:46 -0300

    feat: implement Repository interface in UserRepository and rename update to save

--- src/modules/user/repositories/contracts/UserRepository.ts
+++ src/modules/user/repositories/contracts/UserRepository.ts
@@ -1,9 +1,11 @@
+import { Repository } from '@shared/core/contracts/Repository';
 import { User } from '../../entities/User';
 
-export abstract class UserRepository {
-  abstract findUniqueById(id: number): Promise<User | null>;
-  abstract create(user: User): Promise<void>;
-  abstract update(user: User): Promise<void>;
-  abstract delete(id: number): Promise<void>;
+export abstract class UserRepository implements Repository<User> {
+  abstract findUnique(id: number): Promise<User | null>;
   abstract findUniqueByName(name: string): Promise<User | null>;
+  abstract create(user: User): Promise<void>;
+  abstract save(user: User): Promise<void>;
+  abstract delete(id: number): Promise<void>;
+  abstract findMany(ctx?: unknown): Promise<User[]>;
 }
