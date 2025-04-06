fac4ab9051c3f95e6958c62e4d7c25d203955c4a
Luan Victor <lvictor.dutra@gmail.com>
2025-04-06 16:59:49 -0300

    feat: implement RefreshTokensRepository interface and update method signatures

--- src/modules/user/repositories/contracts/RefreshTokenRepository.ts
+++ src/modules/user/repositories/contracts/RefreshTokenRepository.ts
@@ -1,9 +1,16 @@
 import { RefreshToken } from '@modules/user/entities/RefreshToken';
+import { Repository } from '@shared/core/contracts/Repository';
 
-export abstract class RefreshTokensRepository {
+export abstract class RefreshTokensRepository
+  implements Repository<RefreshToken>
+{
+  abstract findMany(ctx?: unknown): Promise<RefreshToken[]>;
+
+  abstract save(data: RefreshToken, ctx?: unknown): Promise<void>;
+
   abstract create(refreshToken: RefreshToken): Promise<void>;
 
-  abstract findUniqueByUserIdAndToken(
+  abstract findUnique(
     userId: number,
     token: string,
   ): Promise<RefreshToken | null>;
